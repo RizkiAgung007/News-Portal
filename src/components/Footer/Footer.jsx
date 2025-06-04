1e07c30a14f7da662f0b483a34ec3af954164001
unknown <client689k@gmail.com>
2025-06-04 15:31:33 +0700

    membuat footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,17 +3,21 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="container flex mx-auto p-4 w-full text-center text-gray-600">
-      <div className="flex flex-col items-center w-1/2">
-        <h1>Portal Berita</h1>
-        <p>
+      <div className="flex flex-col items-center w-1/4">
+        <div className="border-b border-gray-400 mb-4 pb-2 w-full">
+          <h1>Portal Berita</h1>
+        </div>
+        <p className="text-justify">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat. Duis aute irure dolor in
           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          pariatur.
         </p>
+      </div>
+      <div>
+          
       </div>
     </div>
   );
