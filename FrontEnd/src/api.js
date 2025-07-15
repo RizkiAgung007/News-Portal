0cd3dfb1a658f5ac9e5b5583c75a302318f8e6a4
unknown <client689k@gmail.com>
2025-07-15 15:13:59 +0700

    update

--- FrontEnd/src/api.js
+++ FrontEnd/src/api.js
@@ -1,13 +1,15 @@
-// import axios from 'axios';
-
-// export const NEWS_API_KEY = import.meta.env.VITE_NEWS_API_KEY;
-
-// const API = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL, 
-// });
-
-// export default API;
+import axios from 'axios';
 
 export const NEWS_API_KEY = import.meta.env.VITE_NEWS_API_KEY;
 export const GNEWS_API_KEY = import.meta.env.VITE_GNEWS_API_KEY;
 export const NEWSDATA_API_KEY = import.meta.env.VITE_NEWSDATA_API_KEY;
+
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL, 
+});
+
+export default API;
+
+// export const NEWS_API_KEY = import.meta.env.VITE_NEWS_API_KEY;
+// export const GNEWS_API_KEY = import.meta.env.VITE_GNEWS_API_KEY;
+// export const NEWSDATA_API_KEY = import.meta.env.VITE_NEWSDATA_API_KEY;
